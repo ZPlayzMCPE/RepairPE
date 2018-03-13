2b72a6e05509ec8f92ec7b3a1be8e56f56bee6de
iZeaoGamer <jameshayward14@icloud.com>
2018-03-13 13:59:15 +0000

    Fixed syntax error.
    Hoping this plugin will work out as it should. :P

--- src/RepairPE/Main.php
+++ src/RepairPE/Main.php
@@ -10,15 +10,13 @@ use pocketmine\inventory\ArmourInventory;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener {
-
+    
 public function onEnable(){
     $this->getLogger()->info("Plugin has been enabled. Let the repairing begin!");
 }
-
 public function onDisable(){
     $this->getLogger()->info("Plugin has been disabled. Did the server stop?");
 }
-
 public function onCommand(CommandSender $sender, Command $command, string $labal, array $args) : bool{
     if(strtolower($command->getName()) === "repair") {
         if($sender->hasPermission("repair.use")){
@@ -26,8 +24,8 @@ public function onCommand(CommandSender $sender, Command $command, string $labal
            $sender->sendMessage(TextFormat::GOLD . "Please use /repair all|hand");
            return true;
        }
-if($args[0] == "all") {
-    if(!sender->hasPermission("repair.all")){
+if ($args[0] == "all") {
+    if ($sender->hasPermission("repair.all")){
     if (!$sender instanceof Player) {
             $sender = getInventory()->getContents();
             
@@ -42,19 +40,22 @@ if($args[0] == "all") {
             $sender->sendMessage(TextFormat:: GREEN . "Including your equipped armour.");
             return true;
     }
-    if($args[0]) == "hand") {
-        if(sender->hasPermission("repair.hand")){
-        if($this->getInventory->getContents() as $item){
+    if ($args[0] == "hand") {
+        if ($sender->hasPermission("repair.hand")){
+        foreach($this->getInventory->getContents() as $item){
         }
            $sender->sendMessage(TextFormat::RED . "This item cannot be repaired because it has not been broken.");
            return true;
         }
-    }
     
-    $sender->getInventory()->getItemInHand()->setDamage(0);
-    $item->setDamage(0);
-    $sender->sendMessage(TextFormat::GREEN . "The item named $item has been repaired succesfully.");
-    return true;
+        $sender->getInventory()->getItemInHand()->setDamage(0);
+        $item->setDamage(0);
+        $sender->sendMessage(TextFormat::GREEN . "The item named $item has been repaired succesfully.");
+        return true;
+        }
+        }
     }
-  }
-}          
+        }
+    }
+}
+}
